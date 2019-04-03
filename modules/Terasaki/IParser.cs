4eac3a33a7270ecd9287bc1ac1d3bafede314d88
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-03 16:03:46 +0200

    Changing API

--- modules/Terasaki/IParser.cs
+++ modules/Terasaki/IParser.cs
@@ -2,22 +2,22 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+using System;
 using System.Collections.Generic;
 using System.IO;
 
 namespace Dolittle.Edge.Terasaki
 {
     /// <summary>
-    /// 
+    /// Defines the parser that is capable of parsing the data coming from Terasaki
     /// </summary>
     public interface IParser
     {
         /// <summary>
-        /// 
+        /// Begin parsing from a continuous <see cref="Stream"/>
         /// </summary>
-        /// <param name="stream"></param>
-        /// <returns></returns>
-        IEnumerable<Channel> Parse(Stream stream);
+        /// <param name="stream"><see cref="Stream"/> to continuously get from</param>
+        /// <param name="callback"><see cref="Action{Channel}">Callback</see> for each <see cref="Channel"/> parsed</param>
+        void BeginParse(Stream stream, Action<Channel> callback);
     }
 }
---
+++ modules/Terasaki/Parser.cs
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using System.IO;
+
+namespace Dolittle.Edge.Terasaki
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IParser"/>
+    /// </summary>
+    public class Parser : IParser
+    {
+        /// <inheritdoc/>
+        public void BeginParse(Stream stream, Action<Channel> callback)
+        {
+            
+        }
+    }
+}

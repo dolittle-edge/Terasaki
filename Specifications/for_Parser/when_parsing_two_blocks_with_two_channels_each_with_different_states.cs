943cb416463a650aea18d1a81d09447244f04d9b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-04-04 13:23:03 +0200

    Fix order of blocks in test

--- Specifications/for_Parser/when_parsing_two_blocks_with_two_channels_each_with_different_states.cs
+++ Specifications/for_Parser/when_parsing_two_blocks_with_two_channels_each_with_different_states.cs
@@ -11,7 +11,7 @@ namespace Dolittle.Edge.Terasaki.for_Parser
         Establish context = () => bytes = new byte[]
             {
                 0x02,                                                   // Begin block
-                0x30, 0x30, 0x31,                                       // 001 block
+                0x30, 0x30, 0x30,                                       // 000 block
                 0x30, 0x30, 0x32,                                       // 2 channels
                 0x20, 0x20, 0x2D, 0x30, 0x2E, 0x31, 0x2C,               // Whitespace state, value -0.1
                 0x52, 0x20, 0x34, 0x32, 0x2E, 0x32, 0x37, 0x30, 0x2C,   // R state, value 42.270
@@ -19,7 +19,7 @@ namespace Dolittle.Edge.Terasaki.for_Parser
                 0x03,                                                   // End block
 
                 0x02,                                                       // Begin block
-                0x30, 0x30, 0x30,                                           // 000 block
+                0x30, 0x30, 0x31,                                           // 001 block
                 0x30, 0x30, 0x32,                                           // 2 channels
                 0x42, 0x20, 0x2d, 0x34, 0x32, 0x2E, 0x32, 0x37, 0x30, 0x2C, // B state, value -42.270
                 0x43, 0x20, 0x30, 0x2E, 0x31, 0x2C,                         // C state, value 0.1

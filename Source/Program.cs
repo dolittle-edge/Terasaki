a95ca393704d1965df91d48447a227b8fe020994
thomas t <Thomas.Toekje@raalabs.com>
2020-03-23 13:41:15 +0100

    Changed owner in license.

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,5 +1,5 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) RaaLabs. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System;
